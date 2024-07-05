3c3449be8c632fe0bfb9ebaac4a4159514f76acb
Adebayo Nurudeen <123829629+Broyoung4@users.noreply.github.com>
2024-07-05 17:09:05 +0000

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,52 @@
-import { useState, useReducer } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+gimport { useState, useReducer } from "react";
+
 import "./App.css";
 
-function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  const [name, setName] = useState('');
+const ACTIONS = {
+  ADD_TASK: 'add_task',
+  
+}
 
+
+//reducer function
+function reducer (state, action) {
+  switch(action.type) {
+    case ACTIONS.ADD_TASK:
+      return {
+        tasks: [...state.task, newTask(action.payload.name)]
+      }
+  }
+}
+
+//newtask function 
+function newTask(name) {
+  return {
+    id: Date.now(),
+    name: name,
+    completed: false
+  }
+}
+
+function App() {
+    const [name, setName] = useState('');
+  
+  const [state, dispatch] = useReducer(reducer, {tasks: []})
+
+  function handleSubmit (e) {
+     e.preventDefault();
+     dispatch({type: ACTIONS.ADD_TASK, payload: {name: name}})
+     setName('')
+  }
+
+console.log(text)
   return (
     <>
-      <form>
-        <input value={name} onChange={(e)=> setName(e.target.value) />
-        <button onSubmit={}>ADD</button>
+      <form onSubmit={handleSubmit}>
+      <input value={name} onChange={(e)=> setName(e.target.value)} />
+        <button>ADD</button>
       </form>
+      
+      <span>{text}</span>
     </>
   );
 }
